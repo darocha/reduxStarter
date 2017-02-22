b9e94d02e67609a97e2447adae676e8c8f340308
Luis <luis@overprint.com.ar>
2017-02-22 18:05:16 -0300

    crea componente stateful

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
+// Componente funcional (no tiene state)
 // const SearchBar = () => {
 // 	return <input />;
 // };
 
+// Componente tipo class (tiene state)
 class SearchBar extends Component {
+	constructor(props) {
+		//'super' se utiliza para traer propiedades de la clase 'Component', de la cual extiende SearchBar
+		super(props);
+
+		this.state = { term: '' };
+	}
+
 	render() {
-		return <input/>;
+		return <input onChange={event => this.setState({ term: event.target.value })}/>;
 	}
 };
 
